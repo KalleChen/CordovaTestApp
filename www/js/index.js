6333c9002b9fc3ccc237ce17131dbcf30b551026
Kalle Chen <kallechen.tw@gmail.com>
2025-03-17 19:45:30 +0800

    feat: basic app open event

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,43 @@ function navigateTo(page) {
   window.location.href = page;
 }
 
-// Cordova device ready event (for future native functionality)
-document.addEventListener("deviceready", function () {
-  console.log("Cordova is ready!");
-});
+function clickProduct() {
+  window.i13n.dispatch("action", {
+    I13N: {
+      action: "ClickProduct",
+      products: [
+        {
+          id: 1,
+        },
+      ],
+    },
+  });
+}
+
+document.addEventListener(
+  "deviceready",
+  function () {
+    var server = cordova.plugins.CorHttpd;
+
+    var config = {
+      www_root: "", // absolute or relative path to your app's root
+      port: 8080, // the port number to use
+      localhost_only: false, // whether to listen on localhost only or all interfaces
+    };
+
+    server.startServer(
+      config,
+      function (url) {
+        console.log("Server is live at " + url);
+        // Now you can use 'http://localhost:8080/' instead of 'file://'
+      },
+      function (error) {
+        console.error("Failed to start server: " + error);
+      }
+    );
+
+    // To stop the server when it's no longer needed
+    // server.stopServer();
+  },
+  false
+);
